d9c6a197e827bf172afe7193553b7cbdc3b80eae
David Fu <david.fu.df@gmail.com>
2024-02-21 23:43:04 -0500

    step3

--- app/main.py
+++ app/main.py
@@ -1,15 +1,24 @@
 # Uncomment this to pass the first stage
 import socket
 
+HTTP_202_RESPONSE = b"HTTP/1.1 200 OK\r\n\r\n"
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
+    host = "localhost"
+    port = 4221
     # Uncomment this to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    server_socket = socket.create_server((host, port), reuse_port=True)
+    conn, addr = server_socket.accept() # wait for client
+    conn.recv(4096)
+
+    with conn:
+        conn.sendall(HTTP_202_RESPONSE)
+
+
 
 
 if __name__ == "__main__":
