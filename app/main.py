12684c21961190096e23c7f3e905e8ac1fba31a9
David Fu <david.fu.df@gmail.com>
2024-03-04 00:01:11 -0500

    update

--- app/main.py
+++ app/main.py
@@ -116,8 +116,12 @@ def handleNewConnection(client_connection):
         else:
             response_status = "404 Not Found"
 
-        HEADERS = f"Content-Type: text/plain\nContent-Length: {len(fileName)}"
-        http_response = f"{HTTP_VERSION} {response_status}{CRLF}{HEADERS}\n{fileName}"
+        with open(filePath) as f:
+
+            HEADERS = f"Content-Type: application/octet-stream\nContent-Length: {len(fileName)}"
+            http_response = (
+                f"{HTTP_VERSION} {response_status}{CRLF}{HEADERS}\n{f.read()}"
+            )
 
     client_connection.sendall(http_response.encode("utf-8"))
     client_connection.close()
